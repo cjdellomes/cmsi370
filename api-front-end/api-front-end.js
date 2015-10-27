3cbc5fc726bca7c8939f1705fce74d6ad9cde272
Chris Dellomes <cjdellomes@gmail.com>
2015-10-27 12:02:21 -0700

    Added champ, champSearch, summonerSearch calls

--- api-front-end/api-front-end.js
+++ api-front-end/api-front-end.js
@@ -1,20 +1,53 @@
 $(function () {
 
-	$("#search-button").click(function () {
+	key = "f4380caf-2694-4e63-94b2-3d14fbaaebf4"
+
+	$("#champ-request").click(function () {
 		$.getJSON(
 			"https://na.api.pvp.net/api/lol/na/v1.2/champion",
 
 			{
-				id : $("#search-term").val(),
-				api_key : "f4380caf-2694-4e63-94b2-3d14fbaaebf4"
+				api_key : key
 			}
 
 		).done(function (result) {
 
-			var ouput = result.id
+			var output = result.champions[0].id;
 
-			$("body").append("<p>Test</p>");
+			$("#champ-request-output").text(output);
 
+		});
+	});
+
+	$("#champ-search").click(function () {
+		$.getJSON(
+
+			"https://na.api.pvp.net/api/lol/na/v1.2/champion/" + $("#champ-search-term").val(),
+			{
+				api_key : key
+			}
+
+		).done(function (result) {
+			var output = result.active;
+
+			$("#champ-search-output").text(output);
+
+		});
+	});
+
+	$("#summoner-search").click(function () {
+		$.getJSON(
+
+			"https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/" + $("#summoner-search-term").val(),
+			{
+				api_key : key
+			}
+
+		).done(function (result) {
+			var standardized = $("#summoner-search-term").val().toLowerCase().replace(/\s+/g, '');
+			var output = result[standardized].id;
+
+			$("#summoner-search-output").text(output);
 		});
 	});
 
