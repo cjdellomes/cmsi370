9b055cedcec4ad779e3251ca07aa17af381d7407
Chris Dellomes <cjdellomes@gmail.com>
2015-10-28 14:27:00 -0700

    Added Summoner rank search

--- api-front-end/api-front-end.js
+++ api-front-end/api-front-end.js
@@ -31,6 +31,7 @@ $(function () {
 			}
 
 		).done(function (result) {
+
 			var output = result.name + " " + result.title;
 
 			$("#champ-search-output").text(output);
@@ -38,7 +39,7 @@ $(function () {
 		});
 	});
 
-    $(".dropdown-menu li a").click(function() {
+    $("#summoner-dropdown li a").click(function() {
 
       $("#summoner-region:first-child").text($(this).text());
       $("#summoner-region:first-child").val($(this).text());
@@ -54,10 +55,35 @@ $(function () {
 			}
 
 		).done(function (result) {
+
 			var standardized = $("#summoner-search-term").val().toLowerCase().replace(/\s+/g, '');
 			var output = result[standardized].id;
 
 			$("#summoner-search-output").text(output);
+
+		});
+	});
+
+	$("#rank-dropdown li a").click(function() {
+
+      $("#rank-region:first-child").text($(this).text());
+      $("#rank-region:first-child").val($(this).text());
+
+  	});
+
+	$("#rank-search").click(function () {
+		$.getJSON(
+
+			"https://" + $("#rank-region:first-child").val() + ".api.pvp.net/api/lol/" + $("#rank-region:first-child").val() + "/v2.5/league/by-summoner/" + $("#rank-search-term").val() + "/entry",
+			{
+				api_key : key
+			}
+
+		).done(function (result) {
+
+			var output = result[$("#rank-search-term").val()][0].tier + " " + result[$("#rank-search-term").val()][0].entries[0].division + " " + result[$("#rank-search-term").val()][0].entries[0].leaguePoints + " lp";
+			$("#rank-search-output").text(output);
+
 		});
 	});
 
