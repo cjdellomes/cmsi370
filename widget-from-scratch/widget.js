3a41a7fef5af85b62b343bb49b9dd7ad4444609b
Chris Dellomes <cjdellomes@gmail.com>
2015-12-12 19:18:47 -0800

    Trashing implemented

--- widget-from-scratch/widget.js
+++ widget-from-scratch/widget.js
@@ -1,5 +1,6 @@
 (function ($) {
     var draggable;
+    var target;
     var trashImg = $("<img />", {
         id: "trash",
         src: "http://vignette3.wikia.nocookie.net/mysims/images/d/d5/Trashcan.jpg/revision/latest?cb=20090313211444",
@@ -13,21 +14,35 @@
         draggable.offset({left: event.pageX, top: event.pageY});
     }
 
+    var trash = function(event) {
+        var trashPosition = $("#trash").offset();
+        var clonePosition = $(this).offset();
+        if (clonePosition.left > trashPosition.left && clonePosition.left < trashPosition.left + 100 && 
+        clonePosition.top > trashPosition.top && clonePosition.top < clonePosition.top + 150) {
+            console.log("Trashed");
+            target.remove();
+            draggable.remove();
+        }
+        else{
+            draggable.remove();
+        }
+    }
+
     $.fn.widget = function () {
         this.mousedown(function (event) {
-            var target = event.target;
-            draggable = $(event.target).clone()
-            .addClass("drag-image")
-            .offset({left: event.pageX, top: event.pageY});
+            target = $(event.target);
+            draggable = target.clone()
+                .addClass("drag-image")
+                .offset({left: event.pageX, top: event.pageY});
 
             $("body")
-            .append(draggable)
-            .mousemove(handle);
+                .append(draggable)
+                .mousemove(handle);
 
-            draggable.mouseup(function (event) {
-                draggable.remove();
-            });
+            draggable.mouseup(trash);
         });
     };
+
     $(".draggables").widget();
+
 }(jQuery));
