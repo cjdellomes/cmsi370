382f8940eb91a56e04b8aef78729d8fcd709f78e
Chris Dellomes <cjdellomes@gmail.com>
2015-12-12 17:57:10 -0800

    Started widget, appends trash image

---
+++ widget-from-scratch/widget.js
@@ -0,0 +1,10 @@
+(function ($) {
+   var trashImg = $("<img />", {
+    id: "trash",
+    src: "http://vignette3.wikia.nocookie.net/mysims/images/d/d5/Trashcan.jpg/revision/latest?cb=20090313211444",
+    alt: "trash",
+    width: "100",
+    height: "150"
+   });
+   trashImg.appendTo("#trash");
+}(jQuery));
